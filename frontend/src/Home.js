42e15715dbf9c3190dfc497f611fc2c8d3fdbbe5
Saif <saifisami2005@gmail.com>
2025-01-07 02:01:56 -0500

    Improved the look of the Homepage.

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,29 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Card, CardBody, CardTitle } from "reactstrap";
-
+import { Card, CardBody, CardTitle, Button } from "reactstrap";
 import "./Home.css";
 
 function Home() {
 
+  if (localStorage.getItem("reload")) {
+    window.location.reload();
+    localStorage.removeItem("reload");
+  }
+
   return (
-    <div>{
-      localStorage.getItem("user-token") ? (
-        <>
-        <h1>Welcome back {localStorage.getItem("user-name")}</h1>
-        <p>All jobs, in one place.</p>
-        </>
+    <div className="home-container">
+      {localStorage.getItem("user-token") ? (
+        <Card className="home-card">
+          <CardBody>
+            <CardTitle tag="h1" className="card-title text-center">
+              Welcome back {localStorage.getItem("user-name")}
+            </CardTitle>
+            <p className="card-text text-center">All jobs, in one place.</p>
+          </CardBody>
+        </Card>
       ) : (
-        <div>
-          <h1>Jobly</h1>
-          <p>All Jobs, in one place.</p>
-          <Link to="login">LOG IN</Link>
-          <Link to="signup">SIGN UP</Link>
-        </div>
-      )
-    }
+        <Card className="home-card">
+          <CardBody>
+            <CardTitle tag="h1" className="card-title text-center">
+              Jobly
+            </CardTitle>
+            <p className="card-text text-center">All Jobs, in one place.</p>
+            <div className="button-container">
+              <Button color="primary">
+                <Link to="/login" className="text-white text-decoration-none">
+                  LOG IN
+                </Link>
+              </Button>
+              <Button color="success">
+                <Link to="/signup" className="text-white text-decoration-none">
+                  SIGN UP
+                </Link>
+              </Button>
+            </div>
+          </CardBody>
+        </Card>
+      )}
     </div>
-  )
+  );
 }
 
 export default Home;
